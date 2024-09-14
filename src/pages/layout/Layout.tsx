a954b57394e90ae1044b048d2606bad55b4f7adb
Raviprakashpatel369 <raviprakashpatel369@gmail.com>
2024-09-14 19:27:30 +0530

    Skeleton Done

---
+++ src/pages/layout/Layout.tsx
@@ -0,0 +1,30 @@
+import { Link, Outlet } from 'react-router-dom';
+import styles from './Layout.module.css'; // Import the CSS module
+
+const Layout = () => {
+  return (
+    <div className={styles.layout}>
+      <header>
+        <nav className={styles.navbar}>
+          <div className={styles.navbarLogo}>
+            <h1>Bluff n Bust</h1>
+          </div>
+          <ul className={styles.navbarLinks}>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+            <li><Link to="/profile/1">Profile</Link></li>
+          </ul>
+        </nav>
+      </header>
+      <main className={styles.mainContent}>
+        <Outlet />
+      </main>
+      <footer className={styles.footer}>
+        <p>&copy; 2024 Niggas and co. All Rights Reserved.</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
